05cdf8d7d32308e34642e56a2be3488a2c5d57f2
KuanYuChen <david819162002@yahoo.com.tw>
2016-03-09 00:22:32 +0800

    assignment3-v1

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,9 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
-  end
+    total=0
+    nums_a[0...-1].reverse.each_with_index{|value,index| (index%2==0)? (value*2>9? total+=2*value-9:total+=2*value) :total+=value }
+    return ((10-(total%10))%10 == nums_a[-1])
+	end
 end
